b65f0f13fd65377c0e093b26aa354f668003b1c4
bayramdev <48856944+bayramdev@users.noreply.github.com>
2022-02-19 14:43:14 +0300

    Add header and update title

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import SourceReference from "./SourceReference";
 const App = () => {
   return (
     <div className="w-full h-full flex flex-col items-center pt-4 gap-8">
+      <h1 className="text-xl">QR Code Scanner React.js App</h1>
       <Reader width="75vmin" />
       <SourceReference />
     </div>
